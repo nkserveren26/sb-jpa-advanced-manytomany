4834c8b53196fc42c857bf6ff8d6a023cfb15a33
nkserveren26 <norinoritoshi@outlook.jp>
2024-05-25 21:59:06 +0900

    CourseテーブルとStudentテーブルのレコードを追加

--- src/main/java/com/luv2code/cluddemo/CluddemoApplication.java
+++ src/main/java/com/luv2code/cluddemo/CluddemoApplication.java
@@ -1,10 +1,7 @@
 package com.luv2code.cluddemo;
 
 import com.luv2code.cluddemo.dao.AppDAO;
-import com.luv2code.cluddemo.entity.Course;
-import com.luv2code.cluddemo.entity.Instructor;
-import com.luv2code.cluddemo.entity.InstructorDetail;
-import com.luv2code.cluddemo.entity.Review;
+import com.luv2code.cluddemo.entity.*;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -23,7 +20,30 @@ public class CluddemoApplication {
 	public CommandLineRunner commandLineRunner(AppDAO appDAO) {
 		return runner -> {
 
+			createCourseAndStudents(appDAO);
+
 		};
+	}
+
+	private void createCourseAndStudents(AppDAO appDAO) {
+
+		// create a course
+		Course tempCourse = new Course("Pacman - How To Score One Million Points");
+
+		// create the students
+		Student tempStudent1 = new Student("John", "Doe", "john@luv2code.com");
+		Student tempStudent2 = new Student("Mary", "Public", "mary@luv2code.com");
+
+
+		// add students to the course
+		tempCourse.addStudent(tempStudent1);
+		tempCourse.addStudent(tempStudent2);
+
+		// save the course and associated students
+		System.out.println("Saving the course: " + tempCourse);
+		System.out.println("associated students: " + tempCourse.getStudents());
+
+		appDAO.save(tempCourse);
 	}
 
 	private void deleteCourseAndReviews(AppDAO appDAO) {
